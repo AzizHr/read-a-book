f6e923c166352aef18bc6e5dfe8152ea8a41b255
AzizHr <azizharkati20@gmail.com>
2025-08-04 18:43:31 +0100

    create routes for authenticating the user [login, register and reset password]

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,44 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.dashboard');
+})->name('dashboard');
+
+Route::get('login', [LoginController::class, 'show'])
+    ->name('login.show');
+
+Route::post('login', [LoginController::class, 'login'])
+    ->name('login');
+
+Route::get('register', [RegisterController::class, 'show'])
+    ->name('register.show');
+
+Route::post('register', [RegisterController::class, 'register'])
+    ->name('register');
+
+Route::post('/logout', LogoutController::class)
+    ->name('logout');
+
+Route::controller(ResetPasswordController::class)
+    ->middleware('guest')
+    ->group(function () {
+
+    Route::get('/forgot-password', 'forgotPassword')
+        ->name('password.request');
+
+    Route::post('/forgot-password', 'sendEmail')
+        ->name('password.email');
+
+    Route::get('/reset-password/{token}', 'resetPassword')
+        ->name('password.reset');
+
+    Route::post('/reset-password', 'updatePassword')
+        ->name('password.update');
 });
